39c65c4d94aaffdaa0e09914defad6598a0a7681
vanomark <rudiger.kaspar@mail.ru>
2024-08-14 12:57:28 +0300

    Add files via upload
    Как на русском писать я хз

--- main.cpp
+++ main.cpp
@@ -4,15 +4,15 @@
 
 int main(void){
 float a, b, c, discriminant;
-printf("Вычисление корней уравнения типа ax^2 + bx + c = 0\n");
-printf("Через пробел введите а, b, c\n");
+printf("An equation of the form ax^2 + bx + c = 0\n");
+printf("Enter а, b, c separated by a space\n");
 scanf("%f %f %f",&a,&b,&c);
 if (a && b){
 discriminant = b*b-4*a*c;
-if (discriminant < 0) printf("Нет действительных корней");
-if (discriminant == 0) printf("Единственный корень %.3f", (-b)/(2*a));
-else printf("Корни %.3f, %.3f",(-b-sqrt(discriminant))/2,(-b+sqrt(discriminant))/2);}
-else if(!a && b) printf("Единственный корень %.3f", (-c)/b);
-else if(!a && !b && !c) printf("Бесконечное количество решений");
-else printf("Нет решений");
+if (discriminant < 0) printf("There is no real solution");
+if (discriminant == 0) printf("The solution %.3f", (-b)/(2*a));
+else printf("Solutions %.3f, %.3f",(-b-sqrt(discriminant))/2,(-b+sqrt(discriminant))/2);}
+else if(!a && b) printf("The solution %.3f", (-c)/b);
+else if(!a && !b && !c) printf("An infinite number of solutions");
+else printf("There is no solution");
 return 0;}
