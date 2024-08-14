bd8d274bdb3e9990b094ca38d299d2da5d24d4f4
vanomark <rudiger.kaspar@mail.ru>
2024-08-14 14:10:23 +0300

    Add files via upload
    добавил случай а != 0 b = 0

--- main.cpp
+++ main.cpp
@@ -14,5 +14,7 @@ if (discriminant == 0) printf("The solution %.3f", (-b)/(2*a));
 else printf("Solutions %.3f, %.3f",(-b-sqrt(discriminant))/2,(-b+sqrt(discriminant))/2);}
 else if(!a && b) printf("The solution %.3f", (-c)/b);
 else if(!a && !b && !c) printf("An infinite number of solutions");
-else printf("There is no solution");
+else if(!a && !b && c) printf("There is no solution");
+else if(a*c>0) printf("There is no real solution");
+else printf("Solutions %.3f, %.3f",-sqrt((-c)/a),sqrt((-c)/a));
 return 0;}
