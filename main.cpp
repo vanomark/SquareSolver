326ea6a2fddcc4745a5f52f714b5e6ba6bd6ef6a
vanomark <rudiger.kaspar@mail.ru>
2024-08-15 13:19:34 +0300

    модульность, табы, пробелы, вся фигня...

--- main.cpp
+++ main.cpp
@@ -1,20 +1,52 @@
-#include <TXLib.h>
 #include <stdio.h>
 #include <math.h>
+#define NOSOLUTION printf("There is no solution\n")
+#define INFSOLUTION printf("An infinite number of solutions\n")
 
-int main(void){
-float a, b, c, discriminant;
-printf("An equation of the form ax^2 + bx + c = 0\n");
-printf("Enter a, b, c separated by a space\n");
-scanf("%f %f %f",&a,&b,&c);
-if (a && b){
-discriminant = b*b-4*a*c;
-if (discriminant < 0) printf("There is no real solution");
-if (discriminant == 0) printf("The solution %.3f", (-b)/(2*a));
-else printf("Solutions %.3f, %.3f",(-b-sqrt(discriminant))/2,(-b+sqrt(discriminant))/2);}
-else if(!a && b) printf("The solution %.3f", (-c)/b);
-else if(!a && !b && !c) printf("An infinite number of solutions");
-else if(!a && !b && c) printf("There is no solution");
-else if(a*c>0) printf("There is no real solution");
-else printf("Solutions %.3f, %.3f",-sqrt((-c)/a),sqrt((-c)/a));
-return 0;}
+void beginning(void);
+void calculation(float a, float b, float c);
+
+int main() {
+    float a = 0.0, b = 0.0, c = 0.0;
+
+    beginning();
+
+    scanf("%f %f %f", &a, &b, &c);
+
+    calculation(a, b, c);
+}
+
+
+void beginning(void) {
+    printf("An equation of the form ax^2 + bx + c = 0\n");
+    printf("Enter a, b, c separated by a space\n");
+}
+
+
+void calculation(float a, float b, float c) {
+    if (a && b) {
+        float discriminant;
+        discriminant = b*b - 4*a*c;
+        if (discriminant < 0)
+            NOSOLUTION;
+
+        if (discriminant == 0)
+            printf("The solution %.3f\n", -b/(2*a));
+
+        else printf("Solutions %.3f, %.3f\n", (-b - sqrt(discriminant))/2, (-b + sqrt(discriminant))/2);
+    }
+    else if(!a && b)
+        printf("The solution %.3f\n", -c/b);
+
+    else if(!a && !b && !c)
+        INFSOLUTION;
+
+    else if(!a && !b && c)
+        NOSOLUTION;
+
+    else if(a*c>0)
+        NOSOLUTION;
+
+    else
+        printf("Solutions %.3f, %.3f", -sqrt(-c/a), sqrt(-c/a));
+}
