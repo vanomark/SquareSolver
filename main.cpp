d2dab1fdd90a0b53d20c399b2d189c49937dafbb
vanomark <rudiger.kaspar@mail.ru>
2024-08-20 19:15:14 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <math.h>
+#include <cassert>
 
 void scan_coeff(double *a, double *b, double *c);
 void print_roots(double a, double b, double c);
 int solve_square_equation(double a, double b, double c, double* x1, double* x2);
 int solve_linear_equation(double b, double c, double* x1);
 void print_roots(double x1, double x2, int root_count);
+int solve(double a, double b, double c, double *x1, double* x2);
 
 const double epsilon = 1e-6;
 const int INF = -1;
@@ -16,16 +18,15 @@ int main()
     printf("Enter a, b, c separated by a space\n");
 
     double a = 0, b = 0, c = 0, x1 = 0, x2 = 0;
-    int root_count = 0;
 
     scan_coeff(&a, &b, &c);
-    if (a)
-        root_count = solve_square_equation(a, b, c, &x1, &x2);
-    else
-        root_count = solve_linear_equation(b, c, &x1);
+    int root_count = solve(a, b, c, &x1, &x2);
     print_roots(x1, x2, root_count);
 }
 
+struct aquation {
+double a, b, c, x1, x2;
+} bubu;
 
 void scan_coeff(double *a, double *b, double *c)
 {
@@ -95,4 +96,12 @@ void print_roots(double x1, double x2, int root_count)
 
         default: printf("Error. Something's wrong");
     }
+}
+int solve(double a, double b, double c, double* x1, double* x2)
+{
+
+    if (a)
+        return solve_square_equation(a, b, c, x1, x2);
+    else
+        return solve_linear_equation(b, c, x1);
 }
