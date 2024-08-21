843310515d659cf026d43eea2aa595c0a6c4f6f0
vanomark <rudiger.kaspar@mail.ru>
2024-08-21 22:03:08 +0300

    kill the boy and let the man be born

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <assert.h>
 #include <math.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 void scan_coeff            (double *a, double *b, double *c);
 void print_roots           (double a, double b, double c);
@@ -9,32 +11,54 @@ int  solve_linear_equation (double b, double c, double* x1);
 void print_roots           (double x1, double x2, int root_count);
 int  solve                 (struct equation *eq);
 void run_test              (struct equation eq);
-void all_tests             (void);
+void run_all_tests         (void);
+void turn_lowercase        (char *str);
 
-const double EPSILON = 1e-6;
-const int    INF     = -1;
+const double EPSILON     = 1e-6;
+const int    INF         =   -1;
+const int    BUFFER_SIZE =   40;
 
 struct equation {
-    double a, b, c, right_x1, right_x2;
-    int right_root_count;
+    double a, b, c, x1, x2;
+    int root_count;
 };
 
 int main()
 {
-    all_tests();
 
-    /*
-    printf("An equation of the form ax^2 + bx + c = 0\n");
-    printf("Enter a, b, c separated by a space\n");
-    struct equation eq1 = {};
-    scan_coeff(&eq1.a, &eq1.b, &eq1.c);
-    eq1.right_root_count = solve(&eq1);
-    print_roots(eq1.right_x1, eq1.right_x2, eq1.right_root_count); */
 
+    printf("A solver of equation of the form ax^2 + bx + c = 0\n\n");
+    printf("%14s %8s %20s\nto start solving  %30s",
+           "Enter \"Start\"", "or", "Enter \"Test\"",
+           "to start testing\n\n" );
+
+    char str[BUFFER_SIZE] = "";
+    scanf("%s",str);
+    turn_lowercase(str);
+
+    if (!strcmp(str,"test"))
+    {
+        run_all_tests();
+
+    } else {
+        printf("\nEnter a, b, c separated by a space\n");
+        struct equation eq1 = {};
+
+        scan_coeff(&eq1.a, &eq1.b, &eq1.c);
+        eq1.root_count = solve(&eq1);
+        print_roots(eq1.x1, eq1.x2, eq1.root_count);
+    }
     return 666;
 }
 
-
+void turn_lowercase(char *str)
+{
+    int i = 0;
+    while (str[i] != '\0') {
+        str[i] = tolower(str[i]);
+        i++;
+    }
+}
 
 void scan_coeff(double *a, double *b, double *c)
 {
@@ -57,13 +81,15 @@ int solve(struct equation *eq)
     assert(eq != NULL);
 
     if (eq->a)
-        return solve_square_equation(eq->a, eq->b, eq->c, &(eq->right_x1), &eq->right_x2);
+        return solve_square_equation(eq->a, eq->b, eq->c, &(eq->x1), &eq->x2);
     else
-        return solve_linear_equation(eq->b, eq->c, &eq->right_x1);
+        return solve_linear_equation(eq->b, eq->c, &eq->x1);
 }
 
 int solve_square_equation(double a, double b, double c, double* x1, double* x2)
 {
+    assert(x1 != NULL);
+    assert(x2 != NULL);
 
     double discriminant = b*b - 4*a*c;
 
@@ -101,50 +127,30 @@ void print_roots(double x1, double x2, int root_count)
 {
     switch(root_count) {
 
-        case INF: printf("An infinite number of solutions");
+        case INF: printf("An infinite number of solutions\n");
                     break;
 
-        case 0: printf("There is no solution");
+        case 0: printf("There is no solution\n");
                     break;
 
-        case 1: printf("The solution %.3lg", x1);
+        case 1: printf("The solution %.3lg\n", x1);
                     break;
 
-        case 2: printf("Solutions %.3lg %.3lg", x1, x2);
+        case 2: printf("Solutions %.3lg %.3lg\n", x1, x2);
                     break;
 
-        default: printf("Error. Something's wrong");
+        default: printf("Error. Something's wrong\n");
     }
 }
 
-void all_tests()
+void run_all_tests()
 {
-    struct equation test[20] = {
-        {  1,   0,   -4,   -2,   2,   2},  // Test 1
-        {  0,   0,    0,    0,   0,  -1},  // Test 2
-        {  0,   0,  -10,    0,   0,   0},  // Test 3
-        {  0,   1,  -10,   10,   0,   1},  // Test 4
-        { 10,   0,  -10,   -1,   1,   2},  // Test 5
-        {  1,   2,   -3,   -3,   1,   2},  // Test 6
-        {0.5,   1, -1.5,   -3,   1,   2},  // Test 7
-        {-10,   0,   -1,    0,   0,   0},  // Test 8
-        {  4,   4,    0,   -1,   0,   2},  // Test 9
-        {  4,   4,    1, -0.5,   0,   1},  // Test 10
-        {  1,  13,  -14,  -14,   1,   2},  // Test 11
-        {  1,   1,    1,    0,   0,   0},  // Test 12
-        {  1,   1,    0,   -1,   0,   2},  // Test 13
-        {  0, -98,    0,    0,   0,   1},  // Test 14
-        {  2,   2,    2,    0,   0,   0},  // Test 15
-        {  2,   2,  0.5, -0.5,   0,   1},  // Test 16
-        {  1,   9,  -10,  -10,   1,   2},  // Test 17
-        { 15,   0,    0,    0,   0,   1},  // Test 18
-        {  3,   0,   -3,   -1,   1,   2},  // Test 19
-        {  0,   0,  -10,    0,   0,   0}   // Test 20
+    struct equation test = {};
 
-    } ;
+    FILE *fp = fopen("test.txt","r");
 
-    for (int i = 0; i < 20; i++)
-        run_test(test[i]);
+    while (fscanf(fp, "%lf %lf %lf %lf %lf %d", &test.a, &test.b, &test.c, &test.x1, &test.x2, &test.root_count) != EOF)
+        run_test(test);
 
 }
 
@@ -158,12 +164,12 @@ void run_test(struct equation eq)
     test_case.c = eq.c;
     int root_count = solve(&test_case);
 
-    if (root_count != eq.right_root_count || test_case.right_x1 != eq.right_x1 || test_case.right_x2 != eq.right_x2) {
-        printf("Test %d: Error \na = %lg, b = %lg, c = %lg,\n"
+    if (root_count != eq.root_count || test_case.x1 != eq.x1 || test_case.x2 != eq.x2) {
+        printf("\nTest %d: Error \na = %lg, b = %lg, c = %lg,\n"
                "Programm: x1 = %lg, x2 = %lg, root_count = %d\n"
-               "Expected: x1 = %lg, x2 = %lg, root_count = %d\n\n",
-                    i, eq.a, eq.b, eq.c, test_case.right_x1, test_case.right_x2, root_count,
-                    eq.right_x1, eq.right_x2, eq.right_root_count);
+               "Expected: x1 = %lg, x2 = %lg, root_count = %d\n",
+                    i, eq.a, eq.b, eq.c, test_case.x1, test_case.x2, root_count,
+                    eq.x1, eq.x2, eq.root_count);
     } else {
         printf("Test %d: Success\n\n", i);
     }
