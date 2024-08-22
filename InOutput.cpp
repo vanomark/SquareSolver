6495eebf9a8a77970cef52c4ab036e4427c564dd
vanomark <rudiger.kaspar@mail.ru>
2024-08-22 18:58:53 +0300

    Add files via upload

---
+++ InOutput.cpp
@@ -0,0 +1,41 @@
+#include "InOutput.h"
+
+void scan_coeff(double *a, double *b, double *c)
+{
+    assert(a != NULL);
+    assert(b != NULL);
+    assert(c != NULL);
+
+    int result = scanf("%lf %lf %lf", a, b, c);
+
+    while (result != 3) {
+        char ch = getchar();
+
+        printf("Error. Try again\n");
+
+        while (ch != '\n' && ch != EOF)
+            ch = getchar();
+
+        result = scanf("%lf %lf %lf", a, b, c);
+    }
+}
+
+void print_roots(double x1, double x2, int root_count)
+{
+    switch(root_count) {
+
+        case INFINITE: printf("An infinite number of solutions\n");
+                    break;
+
+        case 0: printf("There is no solution\n");
+                    break;
+
+        case 1: printf("The solution %.3lg\n", x1);
+                    break;
+
+        case 2: printf("Solutions %.3lg %.3lg\n", x1, x2);
+                    break;
+
+        default: printf("Error. Something's wrong\n");
+    }
+}
