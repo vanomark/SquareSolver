c04c8eae8b468d8a0e9cb58ed2a16dbf0fcc31b8
vanomark <rudiger.kaspar@mail.ru>
2024-08-23 20:18:21 +0300

    One ring to bring them all

--- main.cpp
+++ main.cpp
@@ -11,38 +11,31 @@
 #include "RunTestFile.h" 
 #include "SolveFile.h"
 
-int main()
-{
+int main(int argc, char *argv[])
+{   
+
     HANDLE color = GetStdHandle (STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(color, YELLOW_SYMBOLS_BLACK_BACKGROUND);
-    printf("A solver of equation of the form ax^2 + bx + c = 0\n\n");
-    printf("%14s %8s %20s\nto start solving  %30s",
-           "Enter \"Start\"", "or", "Enter \"Test\"",
-           "to start testing\n\n");
-    SetConsoleTextAttribute(color, CYAN_SYMBOLS_BLACK_BACKGROUND);
+    printf("A solver of equation of the form ax^2 + bx + c = 0\n");
+    char *str = argv[1];   
+    turn_lowercase(str);
 
-    char str[BUFFER_SIZE] = "";
-    do {
-        scanf("%s",str);
-        turn_lowercase(str);
+    if (!strcmp(str,"test")) {
+        printf("Let's test this son of a bitch!\n\n");
+        for (int i = 0; i < 1e9; i++);
+        run_all_tests(TESTS_FILE_NAME);
 
-        if (!strcmp(str,"test"))
-        {
-            run_all_tests(TESTS_FILE_NAME);
+    } else {
+        SetConsoleTextAttribute(color, YELLOW_SYMBOLS_BLACK_BACKGROUND);
+        printf("Enter a, b, c separated by a space\n\n");
+        SetConsoleTextAttribute(color, CYAN_SYMBOLS_BLACK_BACKGROUND);
 
-        } else if (!strcmp(str,"start")) {
-            SetConsoleTextAttribute(color, YELLOW_SYMBOLS_BLACK_BACKGROUND);
-            printf("\nEnter a, b, c separated by a space\n");
-            SetConsoleTextAttribute(color, CYAN_SYMBOLS_BLACK_BACKGROUND);
+        struct equation eq1 = {};
 
-            struct equation eq1 = {};
-
-            scan_coeff(&eq1.a, &eq1.b, &eq1.c);
-            eq1.root_count = solve(&eq1);
-            print_roots(eq1.x1, eq1.x2, eq1.root_count);
-        }
-    } while (strcmp(str,"test") && strcmp(str,"start"));
-
+        scan_coeff(&eq1.a, &eq1.b, &eq1.c);
+        eq1.root_count = solve(&eq1);
+        print_roots(eq1.x1, eq1.x2, eq1.root_count);
+    }
     return 0; 
 }
 
