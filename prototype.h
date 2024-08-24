b7112b43a0497e3ffa78da4bee74dd6bd16de932
vanomark <rudiger.kaspar@mail.ru>
2024-08-24 14:12:13 +0300

    Delete prototype.h

--- prototype.h
+++
@@ -1,8 +0,0 @@
-#include <windows.h>
-void   scan_coeff            (double *a, double *b, double *c);
-void   print_roots           (double x1, double x2, int root_count);
-int    solve                 (struct equation *eq);
-void   run_all_tests         (const char *NameOfFile);
-void   turn_lowercase        (char *str);
-
-const int    BUFFER_SIZE =   40;
